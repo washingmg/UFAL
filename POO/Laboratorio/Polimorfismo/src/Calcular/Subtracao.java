f1ba3033dec13a0a633887288cc97ef35a5eb1e2
Washington Medeiros <wmedeirosm001@gmail.com>
2024-08-15 15:54:49 -0300

    Update

--- POO/Laboratorio/Polimorfismo/src/Calcular/Subtracao.java
+++ POO/Laboratorio/Polimorfismo/src/Calcular/Subtracao.java
@@ -6,8 +6,8 @@ public class Subtracao extends Operacao {
     public void calcular(float num1, float num2) {
         float valor;
             
-        valor = num1 * num2;
-        System.out.println("Você dividiu " + num1 + " - "+ num2);
-        System.out.println("Resultando em: "+ valor);
+        valor = num1 - num2;
+        this.mostrar("Você dividiu " + num1 + " - "+ num2);
+        this.mostrar("Resultando em: "+ valor);
     }
 }
