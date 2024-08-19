dfa20ed729313558c8936232c6bef98038d754fb
Washington Medeiros <wmedeirosm001@gmail.com>
2024-08-19 09:10:34 -0300

    Update

--- POO/Encapsulamento/src/aula2/poo/pacote2/Principal.java
+++ POO/Encapsulamento/src/aula2/poo/pacote2/Principal.java
@@ -1,16 +1,21 @@
 package Encapsulamento.src.aula2.poo.pacote2;
 
-// import Encapsulamento.src.aula2.poo.pacote1.Carro;
+import Encapsulamento.src.aula2.poo.pacote1.Carro;
 import Encapsulamento.src.aula2.poo.pacote1.Motorista;
 
 public class Principal {
     public static void main(String[] args) {
-        CarroEletrico c1 = new CarroEletrico("ABC-1234", "Uno", "FIAT");
-        CarroCombustao c2 = new CarroCombustao("AAA-1111", "Gol", "VW");
+        Carro c1 = new CarroEletrico("ABC-1234", "Uno", "FIAT");
+        Carro c2 = new CarroCombustao("AAA-1111", "Gol", "VW");
 
+        CarroEletrico ce = (CarroEletrico)c1; //typecast
+        CarroCombustao cc = (CarroCombustao)c2;
+
+        ce.carregar(30);
         c1.mostrar();
         System.out.println(c1);
 
+        cc.abastecer(50);
         c2.mostrar();
         System.out.println(c2);
 
@@ -19,11 +24,10 @@ public class Principal {
 
         c1.setMotorista(joao);
         joao.setCarro(c1);
-        
+        c1.mostrar();
+
         c2.setMotorista(maria);
         maria.setCarro(c2);
-
-        c1.mostrar();
         c2.mostrar();
     }
 }
