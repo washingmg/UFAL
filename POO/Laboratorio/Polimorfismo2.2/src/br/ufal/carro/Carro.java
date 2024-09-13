cd55d2532c05c3a3d1e83c38dab8754d97bac0be
Washington Medeiros <wmedeirosm001@gmail.com>
2024-09-13 16:30:10 -0300

    Polimorfismo de Inclusao, Classes Abstratas e Interfaces

--- POO/Laboratorio/Polimorfismo2.2/src/br/ufal/carro/Carro.java
+++ POO/Laboratorio/Polimorfismo2.2/src/br/ufal/carro/Carro.java
@@ -15,55 +15,63 @@ public class Carro {
         this.velocidade = 0;
         this.tanque = tanque;
     }
-
+    
     public void ligar() {
-		if (!motor.isLigado()) {
-            if (tanque.getQuantidadePresente() > 0) {
-                motor.ligar();
-                System.out.println("O carro " + modelo +  " de cor " + cor + " foi ligado.");
+		if (!this.motor.isLigado()) {
+            if (this.tanque.getQuantidadePresente() > 0) {
+                this.motor.ligar();
+                System.out.println("O carro " + this.getModelo()+  " de cor " + this.getCor() + " foi ligado.");
             } else System.out.println("Sem combustível para ligar!");
         } else System.out.println("Motor já ligado!");
     }
 
     public void desligar() {
-        if (motor.isLigado()) {
+        if (this.motor.isLigado()) {
             if (getVelocidade() > 0) {
                 this.freiar();
             }
-                motor.desligar();
-                System.out.println("O carro " + modelo +  " de cor " + cor + " foi desligado.");
+            this.motor.desligar();
+                System.out.println("O carro " + this.getModelo()+  " de cor " + this.getCor() + " foi desligado.");
             } else System.out.println("Já desligado!");
         } 
 
     public void acelerar(int quantCombustivel) {
-        int combustivelDisponivel = tanque.usarCombustivel(quantCombustivel);
-        if (motor.isLigado()) {
-            if (combustivelDisponivel > 0) {
-                motor.acelerar(this, combustivelDisponivel);
-                if (tanque.getQuantidadePresente() == 0) {
-                    System.out.println("Faltou combustível :/.");
-                    desligar(); 
-                    }
-                } else {
-                    motor.desligar();
+    int combustivelDisponivel = tanque.usarCombustivel(quantCombustivel);
+    if (this.motor.isLigado()) {
+        if (combustivelDisponivel > 0) {
+            this.motor.acelerar(this, combustivelDisponivel);
+            if (this.tanque.getQuantidadePresente() == 0) {
+                System.out.println("Faltou combustível :/.");
+                this.desligar(); 
                 }
+            } else {
+                this.motor.desligar();
             }
         }
-
+    }
+    
     public void freiar() {
-        velocidade = 0;
+        this.velocidade = 0;
         System.out.println("Carro freiado!");
     }
-
+    
     protected void setVelocidade(int velocidade) {
         this.velocidade = velocidade;
     }
-
+    
     public int getVelocidade() {
         return velocidade;
     }
-
+    
     public void abastecer(int quantidade) {
-        tanque.abastecer(quantidade);
+        this.tanque.abastecer(quantidade);
+    }
+    
+    public String getCor() {
+        return cor;
+    }
+
+    public String getModelo() {
+        return modelo;
     }
 }
--- POO/Laboratorio/Polimorfismo2.2/src/br/ufal/carro/Tanque.java
+++ POO/Laboratorio/Polimorfismo2.2/src/br/ufal/carro/Tanque.java
@@ -10,21 +10,21 @@ public class Tanque {
     }
 
     public void abastecer(int quantidade) {
-        if (quantidade + quantidadePresente <= capacidade) {
-            quantidadePresente += quantidade;
-            System.out.println("Abastecendo " + quantidade + " litros. Combustível presente: " + quantidadePresente + " litros.");
+        if (quantidade + this.getQuantidadePresente() <= this.capacidade) {
+            this.quantidadePresente += quantidade;
+            System.out.println("Abastecendo " + quantidade + " litros. Combustível presente: " + this.getQuantidadePresente() + " litros.");
         } else {
             System.out.println("Não é possível abastecer. Quantidade além da capacidade do tanque.");
         }
     }
 
     public int usarCombustivel(int quantidade) {
-        if (quantidadePresente >= quantidade) {
-            quantidadePresente -= quantidade;
+        if (this.getQuantidadePresente() >= quantidade) {
+            this.quantidadePresente -= quantidade;
             return quantidade;
         } else {
-            int quantidadeUsada = quantidadePresente;
-            quantidadePresente = 0;
+            int quantidadeUsada = this.quantidadePresente;
+            this.quantidadePresente = 0;
             return quantidadeUsada;
         }
     }
