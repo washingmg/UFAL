767dd98a76048ccb3d00b1c85d1f6753b72acfb3
Washington Medeiros <wmedeirosm001@gmail.com>
2024-10-09 10:33:50 -0300

    Solução q2

--- APC/Provas/Ab1/ab1.1/q2.py
+++ APC/Provas/Ab1/ab1.1/q2.py
@@ -1,15 +1,12 @@
-def verificar_aposentadoria(idade, anos_trabalho):
-    if idade >= 65:
-        return "Pode se aposentar"
-    elif anos_trabalho >= 30:
-        return "Pode se aposentar"
-    elif idade >= 60 and anos_trabalho >= 25:
-        return "Pode se aposentar"
-    else:
-        return "Não pode se aposentar"
 
 idade = int(input("Digite a idade do empregado: "))
 anos_trabalho = int(input("Digite o número de anos trabalhados: "))
 
-resultado = verificar_aposentadoria(idade, anos_trabalho)
-print(resultado)
+if idade >= 65:
+    print("Pode se aposentar")
+elif anos_trabalho >= 30:
+    print("Pode se aposentar")
+elif idade >= 60 and anos_trabalho >= 25:
+    print("Pode se aposentar")
+else:
+    print("Não pode se aposentar")
