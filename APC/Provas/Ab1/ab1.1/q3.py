649827b63ff220685d3861fa81c26a11ed16ee17
Washington Medeiros <wmedeirosm001@gmail.com>
2024-10-09 10:33:20 -0300

    Solução q3

--- APC/Provas/Ab1/ab1.1/q3.py
+++ APC/Provas/Ab1/ab1.1/q3.py
@@ -1,15 +1,16 @@
-def calcular_peso_ideal(nome, altura, sexo):
-    if sexo.upper() == 'M':
-        peso_ideal = (72.7 * altura) - 58
-    elif sexo.upper() == 'F':
-        peso_ideal = (62.1 * altura) - 44.7
-    else:
-        return "Sexo inválido"
-    return f"{nome}, seu peso ideal é {peso_ideal:.2f} kg"
-
 
 nome = input("Digite o nome da pessoa: ")
 altura = float(input("Digite a altura da pessoa (em metros): "))
 sexo = input("Digite o sexo da pessoa (M ou F): ")
 
-print(calcular_peso_ideal(nome, altura, sexo))
+if sexo.upper() == 'M':
+    peso_ideal = (72.7 * altura) - 58
+elif sexo.upper() == 'F':
+    peso_ideal = (62.1 * altura) - 44.7
+else:
+    peso_ideal = None
+
+if peso_ideal is not None:
+    print(f"{nome}, seu peso ideal é {peso_ideal} kg")
+else:
+    print("Sexo inválido")
